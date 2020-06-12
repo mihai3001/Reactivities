822d620f6fb29c3c3f1fe5864a1ddb9a480bf50e
mihai3001 <rider.white26@yahoo.com>
2020-06-12 23:52:43 +0300

    End of Section 16

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -8,5 +8,7 @@ namespace Domain
     {
         public string DisplayName {get; set;}
         public virtual ICollection<UserActivity> UserActivities { get; set; }
+        public string Bio { get; set; }
+        public virtual ICollection<Photo>  Photos{get; set;}
     }
 }
